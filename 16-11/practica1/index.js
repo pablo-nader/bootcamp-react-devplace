f2bc3429a836b82ef9d5fea803f143643460499f
Pablo Matias Nader <hm.pablito@gmail.com>
2021-11-21 20:01:05 -0300

    [ADD] practica 1

--- 16-11/practica1/index.js
+++ 16-11/practica1/index.js
@@ -5,7 +5,8 @@
  */
 const pColor = document.querySelector("#pColor");
 const paragraph = document.querySelectorAll('p');
-    
+var textWords = "";
+let count;
 
 pColor.addEventListener('change', function () {
     Array.from(paragraph).forEach(e => e.style.backgroundColor = pColor.value);
@@ -16,6 +17,27 @@ pColor.addEventListener('change', function () {
  * Contar la cantidad de palabras dentro de los elementos <p> e insertar al principio de la página la cantidad
  */
 
+const button = document.querySelector("#buttonCount");
+const display = document.querySelector("#displayCount");
+
+button.addEventListener('click', function() {
+    Array.from(paragraph).forEach((e, i) => {
+        let text = e.innerHTML;
+        // reemplazamos saltos de linea, por espacios, 
+        // varios espacios por uno solo
+        // sacamos espacios del principio y del final
+        text = text.replace (/\r?\n/g," ");
+    	text = text.replace (/[ ]+/g," ");
+        text = text.replace (/^ /,"");
+        text = text.replace (/ $/,"");
+        count = text.split(" ").length;
+        
+        textWords += `<br>En el párrafo ${i+1} hay ${count} letras`;
+    });
+
+    display.innerHTML = textWords;
+});
+
 /**
  * Ejercicio Nº3:
  * Crear un anchor que el link sea https://www.google.com.ar/
@@ -23,7 +45,22 @@ pColor.addEventListener('change', function () {
  * muestre la dirección en un elemento del html
  */
 
+const anchor = document.querySelector("#unclickeable-anchor");
+
+anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('displayURL').innerHTML = "<br>La URL actual es: " + location;
+});
+
 /**
  * Ejercicio Nº4:
  * Crear un elemento <img> que contenga una imagen a elección
  */
+
+const buttonImg = document.querySelector("#buttonImg");
+
+buttonImg.addEventListener('click', function () {
+    let img = document.createElement('img');
+    img.src = 'https://picsum.photos/200/200';
+    document.getElementById('displayImg').appendChild(img);
+});
